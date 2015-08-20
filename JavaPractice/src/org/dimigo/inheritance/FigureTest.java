a00cede69647090cc89dadc636a078ea0a16b0ce
codertimo <codertimo@gmail.com>
2015-08-20 11:35:33 +0900

    dd

--- JavaPractice/src/org/dimigo/inheritance/FigureTest.java
+++ JavaPractice/src/org/dimigo/inheritance/FigureTest.java
@@ -11,7 +11,14 @@ public class FigureTest {
         Rectangle r = new Rectangle(20,20,5,8);
 
         System.out.println("원의 넓이 :"+String.format("%.1f",c.calcArea()));
-        System.out.println("삼각형의 넓이 :"+String.format("%.1f", t.calcArea()));
+        System.out.println("삼각형의 넓이 :" + String.format("%.1f", t.calcArea()));
         System.out.println("사각형의 넓이 :" + String.format("%.1f", r.calcArea()));
+
+        c.printCenter();t.printCenter();r.printCenter();
+        c.moveFigure(5,5);t.moveFigure(5,5);r.moveFigure(5,5);
+        System.out.println("-- 중심좌표 이동 (x,y축 5칸씩)");
+        c.printCenter();t.printCenter();r.printCenter();
+
+
     }
 }
