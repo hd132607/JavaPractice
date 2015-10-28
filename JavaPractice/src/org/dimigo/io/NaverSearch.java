8939757db074dafd1f53cdde3aa235e8aefd819c
codertimo <codertimo@gmail.com>
2015-10-28 16:44:42 +0900

    IO 실습

--- JavaPractice/src/org/dimigo/io/NaverSearch.java
+++ JavaPractice/src/org/dimigo/io/NaverSearch.java
@@ -18,15 +18,29 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 /**
+ * Made by : Jusneong Kim
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2015 Junseong Kim
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ */
+
+
+/**
  * <pre>
  * org.dimigo.io
  *   |_ NaverSearch
  * 
- * 1. 개요 : 
- * 2. 작성일 : 2015. 9. 28.
+ * 1. 개요 : 네이버 영화 API/File IO
+ * 2. 작성일 : 2015. 10.28
  * </pre>
  *
- * @author		: teacher
+ * @author		: 김준성
  * @version		: 1.0
  */
 public class NaverSearch {
