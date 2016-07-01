a280856513773f388337fc5a31de53972161628c
Junseong Kim <codertimo@gmail.com>
2016-07-01 13:21:42 +0900

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,116 @@
+//기본 문법
+<script language= "javascript" src="abc.js">
+document.write(“line 1 <br>”);
+
+//1. 특수문자는 _과 $만 허용된다.
+//2. 공백문자를 포함할 수 없다.
+//3. 숫자로 시작할 수 없다.
+
+typeof(a)
+
+//1. 더하기는 문자열이 있으면 무조건 문자열로 ㄱㄱ
+alert('52' + 273); -> '52273'
+alert(52 + '273'); -> '52273'
+
+//2. 곱하기는 숫자를 곱함.
+alert('52' * 273); -> 14196
+
+// == : 동등연산자
+alert(1 == "1"); //T
+alert(0 == false); //T
+alert('' == false); //T
+alert(null == false); //F
+alert('' == null); //F
+
+// === 항등연산자 : 같음 연산자와 동일하게 동작하지만 형식 변환이 수행 되지 않습니다. 두 식의 형식이 같지 않으면 항상 false 를 반환합니다.
+alert(1 === "1"); //F
+alert(0 === false); //F
+alert('' === false); //F
+alert(null === false); // F
+alert('' === null); //F
+
+//1~6 난수 생성
+Math.floor(Math.random() * 6)+1
+
+
+//document에서 객체 가져오기
+var fromNum = Number(document.frmAcc.from.value);
+<body>
+  <h3>자바스크립트 연산</h3>
+  <form method="POST" name="frmAcc">
+  <input type="text" name="from" value="1" size="4" />부터
+  </form>
+</body>
+
+
+//For 문 사용법
+var array = [273, 32, 'String', true, [123, 124]];
+// 배열의 순회 방법1
+for (i = 0; i < array.length; i++)
+  alert(array[i]);
+// 배열의 순회 방법2
+for (var i in array)
+  alert(array[i]);
+
+
+//window.confirm()
+var ans = window.confirm('계속 진행할까요?');
+if(ans == true)
+  document.write('continue...');
+else
+  document.write('stopped');
+
+//window.open()
+window.open('http://m.daum.net', '','width=400, height=600, scrollbars=yes, toolbar=yes');
+//window.close()
+// <input type="button" value="닫기" onClick=“window.close();"/>
+
+//객체생성
+var product = {
+  제품명: '7D 건조 망고',
+  유형: '당절임',
+  원산지: '필리핀'
+}
+
+//객체 접근
+product['제품명'] == product.제품명
+//for문에서 객체 접근
+for(var key in product) {
+  output += '● ' + key + ': ' + product[key] + '\n';
+}
+
+var person = { // 객체 정의
+  name: '최은국',
+  eat: function (food) {
+    // 멤버변수 접근 시 this 생략 불가능
+    alert(this.name + '이 ' + food + '을/를 먹습니다.');
+  }
+};
+
+// 동적으로 속성 추가
+var student = {}; // 빈 객체 생성
+student.이름 = '이주진';
+student.개인기 = '병혁샘 목소리 흉내';
+student.장래희망 = 'IT CEO';
+student.toString = function (){...}
+
+//객체 속성 제거
+delete(student.개인기);
+//객체 생성자
+function Student(name, kor, mat, eng, sci){
+  this.name =name;
+  ...
+}
+//객체 생성
+new Student('강민수', 80, 81, 82, 83)
+
+//프로토 타입과 일반 함수(중복되서 메모리를 계속 차지함)
+this.getSum = function () {
+  return this.국어 + this.수학 + this.영어 + this.과학;
+}
+Student.prototype.getSum = function () {
+  return this.국어 + this.수학 + this.영어 + this.과학;
+}
+
+상속
+Square.prototype = Rectangle.prototype;
---
+++ Jquery.js
@@ -0,0 +1,142 @@
+
+var getter = $('h3').css('text-align'); //css값 가져오기
+
+$('h1').css('color','red');
+//모든 h1에있는 자식 p의 배경 변경
+$('h1 p').css('background-color','yellow');
+//h1밑에 직계로 있는 자식만 변경
+$('h1 > p').css(...)
+
+//입력값중 체크된것
+$('input:checked').val()
+
+//테이블에서 Jquery 선택
+$('tr:odd').css(...) //홀수 번째 선택
+$('tr:even').css(...) //짝수 번째
+$('tr:first').css(...)
+
+$('h3:contians(문자열)').css(...) //특정 문자열 포함
+$('h3:eq(n)') //n번째 엘리먼트 선택 (음수면 끝에서부터 셈)
+$('h3:gt(n)') //n번째이후의 모든 엘리먼트 선택
+$('h3:lt(n)') //n번째이전의 모든 엘리먼트 선택
+$('h3:has(h1)') //h1테그를 갖고있는 엘리먼트 선택
+$('body:not(h1)') //h1이 아닌 엘리먼트들 선택
+$('tr:nth-child(3n+1)') //3n+1번째 엘리먼트들 선택
+
+$('h3').filter(':odd').css('color','black').end().
+       .filter(':even').css('color','yellow')
+
+$('h1').addClass('red-man') //테그에 클래스를 추가함
+$('h1').removeClass('red-man') //제거
+$('h1').toggleClass('red-man') //지정되어있으면 제거, 있지 않ㅇ면 추가
+
+
+$('img').attr('src');//src의 값 가져오기
+$('img').attr('width', 150); //width값 설정하기
+
+var btn = $('#switch').val();
+$('#switch').val('OFF'); //Value값 설정
+
+
+var color = ['red', 'blue', 'green'];
+//h2의 index를 넘겨주는거 같은데...
+$('h2').css('color', function(index)
+{
+  return color[index];
+});
+
+var str_html = $('div').html() // 결과 -> <h2>Hello</h2>
+var str_text = $('div').text() // 결과 -> Hello
+
+$('div').html('<h2>Add HTML</h2>')
+$('div').text('Add TEXT')
+
+
+$('div').remove() //객체삭제
+$('div').empty() //내부 자손 삭제
+
+
+$(A).appendTo(B) //A를 B의 뒷부분에 추가
+$(A).prependTo(B) //A를 B의 앞부분에 추가
+$(A).insertAfter(B) //A를 B의 뒤에 추가
+$(A).insertBefore(B) //A를 B의 앞에 추가
+
+$(A).append(B) //B를 A의 뒷부분에 추가
+$(A).prepend(B) //B를 A의 앞부분에 추가
+$(A).after (B) //B를 A의 뒤에 추가
+$(A).before(B) //B를 A의 앞에 추가
+
+$(A).clone() //객체 복사
+
+var content = [
+  { name: '윤인성', region: '서울특별시 강서구' },
+  { name: '윤하린', region: '서울특별시 광진구' },
+  { name: '윤인아', region: '서울특별시 강남구' }
+];
+
+//이렇게 함수로 HTML을 넘겨줘도 가능
+$('div').append(function(index) {
+  var item = content[index];
+  var str = '';
+  str += '<h3>'+ item.name +'</h3>';
+  str += '<p>'+ item.region +'</p>';
+  return str;
+});
+
+//bind()함수 : 이벤트 처리 함수 ('이벤트 명', 실행할함수)
+$('input').bind('focusin', function () {
+  $(this).css('background-color', 'yellow');
+})
+
+//객체로 여러개 념겨줄수도 있음
+$('h2').bind({
+  mouseenter: function() { $(this).css('color', 'red'); },
+  mouseleave: function() { $(this).css('color', 'black'); }
+})
+
+//이런식으로 바로 사용도 가능
+$('h2').click(function(event){
+  ...
+})
+
+
+$(selector).unbind(); // 객체와 관련된 모든 이벤트 제거
+$(selector).unbind(eventname); // 특정 이벤트와 관련된 모든 핸들러 제거
+$(selector).unbind(eventname, function); // 특정 이벤트 핸들러를 제거
+
+$(selector).one() //일회용 이벤트 : 한번 사용후 종료됨
+
+$('div').click(function(event){
+  //클릭된 div내에 있는 h2의 색을 바꿈
+  $('h2',this).css('color'.red);
+})
+
+$(selector).trigger('click') // 강제 이벤트 발생
+$('h2').each(function(index){...}); //for-each문
+
+//Key Event
+keydown : "키보드가 눌렸을때"
+keyup : "키보드가 떼어질때"
+keypress : "글자가 입력될때"
+
+//내부저장소
+localStorage.setItem('tag','text');
+localStorage.getItem('tag');
+
+
+//윈도우 이벤트
+'ready' : 문서 객체가 준비를 완료할 때 발생합니다.
+'load' : 문서 객체를 불러들일 때 발생합니다.
+'unload' :문서 객체를 닫을 때 발생합니다.
+'resize' : 크기를 변화 시킬 때 발생합니다.
+'scroll' :스크롤 할 때 발생합니다.
+'error' :에러가 있을 때 발생합니다.
+
+$(selector).preventDefault() //기본 이벤트 (예 : a테그 링크 이동) 제거
+$(selector).stopPropagation() //이벤트 전달을 제거합니다
+
+//bind의 한계점 : 객체를 clone했을때 이벤트 헨들러까지 복제가 되지 않음 따라서 on()함수를 사용함.
+
+$('h1').on('mousedown',function(event){...}); //모든 h1에 대해서 이벤트 적용
+
+$('div').on('mousedown','h1',function(event){$(this).css('...')}); //div가 클릭되면 div안에 있는 h1에 대해서 css변경
